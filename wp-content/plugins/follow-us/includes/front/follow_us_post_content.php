edfdba7148866e96705640d70233cabc3ea2fbe8
Bohdan <bogmira777@gmail.com>
2023-05-27 13:26:30 +0200

    follow us plugin fix

--- wp-content/plugins/follow-us/includes/front/follow_us_post_content.php
+++ wp-content/plugins/follow-us/includes/front/follow_us_post_content.php
@@ -4,8 +4,5 @@ function follow_us_to_post_content($content)
     if(is_single()){
         return '<div class="follow-us"><a href="https://www.instagram.com/bogsvity_777/" target="_blank">'.__('Follow us').'<i class="fa fa-heart"></i></a></div>' . $content;
     }
-    // if(is_home()){
-    //     return 
-    // }
     return $content;
 }
